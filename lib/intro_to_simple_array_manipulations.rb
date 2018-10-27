e0a36ecb55957fe64e290b69dc1286423f0b1e4e
sun5hine <af5kh@yahoo.com>
2018-10-27 21:40:10 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -8,16 +8,19 @@ end
 
 def using_pop(arr)
   arr.pop 
+  arr
 end
 
 def pop_with_args(arr)
    using_pop
    arr.pop
+   arr
  end 
  
  def using_shift(arr)
    arr.shift 
+   arr
  end
  
- def 
+
   
