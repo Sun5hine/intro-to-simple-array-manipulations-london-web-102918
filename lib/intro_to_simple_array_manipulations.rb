2b6af6c56855ec08d6625f49f4e3224dae0353f2
sun5hine <af5kh@yahoo.com>
2018-10-27 21:35:34 +0000

    automatically backed up by learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_push(arr, string)
+  arr.push(string)
+end
+
+def using_unshift(arr, string)
+  arr.unshift(string)
+end
+
+def using_pop(arr)
+  arr.pop 
+end
+
+def pop_with_args(arr)
+   using_pop
+   arr.pop
+ end 
+ 
+ def using_shift(arr)
+   arr.shift 
+ end
+ 
+ def 
+  
